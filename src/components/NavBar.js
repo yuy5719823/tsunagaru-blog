4e2457bea646e6412bd5828e604de63d6801f588
yuy5719823 <youya_827@yahoo.co.jp>
2025-05-15 20:03:56 +0900

    ナビゲーションバー

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,12 +1,26 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse, faFilePen, faArrowRightToFile } from "@fortawesome/free-solid-svg-icons";
 
 export const NavBar = () => {
   return (
     <nav>
-      <Link to="/">ホーム</Link>
-      <Link to="/createpost">記事投稿</Link>
-      <Link to="/login">ログイン</Link>
+      <Link to="/">
+        <FontAwesomeIcon icon={faHouse} />
+        ホーム
+      </Link>
+      <Link to="/createpost">
+        {" "}
+        <FontAwesomeIcon icon={faFilePen} />
+        記事投稿
+      </Link>
+      <Link to="/login">
+        {" "}
+        <FontAwesomeIcon icon={faArrowRightToFile} />
+        ログイン
+      </Link>
     </nav>
   );
 };
