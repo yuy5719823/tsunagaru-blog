641a76fedee80b4ce0d9a71ac31728809994499c
yuy5719823 <youya_827@yahoo.co.jp>
2025-05-17 08:47:36 +0900

    投稿ページのCSSまで

--- src/components/CreatePost.js
+++ src/components/CreatePost.js
@@ -1,5 +1,20 @@
-import React from "react";
+import "./CreatePost.css";
 
 export const CreatePost = () => {
-  return <div>CreatePost</div>;
+  return (
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className="inputPost">
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入" />
+        </div>
+        <div className="inputPost">
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を記入"></textarea>
+        </div>
+        <button className="postButton">投稿する</button>
+      </div>
+    </div>
+  );
 };
